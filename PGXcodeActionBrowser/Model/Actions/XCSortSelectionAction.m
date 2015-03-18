b4d98ceb4f016c95a8352fafcbe81ef589d87834
Pedro Gomes <pdcgomes@gmail.com>
2015-03-18 00:22:21 +0000

    [REFACTORING] Setting up the comparison function on init

--- PGXcodeActionBrowser/Model/Actions/XCSortSelectionAction.m
+++ PGXcodeActionBrowser/Model/Actions/XCSortSelectionAction.m
@@ -16,6 +16,7 @@
 @interface XCSortSelectionAction ()
 
 @property (nonatomic, assign) NSComparisonResult sortOrder;
+@property (nonatomic,   copy) NSComparator compareFunction;
 
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *subtitle;
@@ -37,6 +38,25 @@
         self.sortOrder = sortOrder;
         self.subtitle  = @"Sorts the selected text";
         self.title     = [NSString stringWithFormat:@"Sort selection (%@)", sortOrder == NSOrderedDescending ? @"descending" : @"ascending"];
+        
+        NSComparator compareFunction  = (self.sortOrder == NSOrderedAscending ?
+                                         ^(NSString *str1, NSString *str2) {
+                                             NSString *trimmedStr1 = [str1 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                                             NSString *trimmedStr2 = [str2 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                                             return [trimmedStr1 compare:trimmedStr2 options:NSNumericSearch];
+                                         } :
+                                         ^(NSString *str1, NSString *str2) {
+                                             NSString *trimmedStr1 = [str1 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                                             NSString *trimmedStr2 = [str2 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                                             
+                                             NSComparisonResult result = [trimmedStr1 compare:trimmedStr2 options:NSNumericSearch];
+                                             switch(result) {
+                                                 case NSOrderedAscending:   return NSOrderedDescending;
+                                                 case NSOrderedDescending:  return NSOrderedAscending;
+                                                 case NSOrderedSame:        return NSOrderedSame;
+                                             }
+                                         });
+        self.compareFunction = compareFunction;
     }
     return self;
 }
@@ -54,25 +74,7 @@
     [lineComponents removeLastObject];
     if(lineComponents.count < 2) return NO; // nothing to sort
 
-    NSComparator compareFunction  = (self.sortOrder == NSOrderedAscending ?
-                                     ^(NSString *str1, NSString *str2) {
-                                         NSString *trimmedStr1 = [str1 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-                                         NSString *trimmedStr2 = [str2 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-                                         return [trimmedStr1 compare:trimmedStr2 options:NSNumericSearch];
-                                     } :
-                                     ^(NSString *str1, NSString *str2) {
-                                         NSString *trimmedStr1 = [str1 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-                                         NSString *trimmedStr2 = [str2 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-
-                                         NSComparisonResult result = [trimmedStr1 compare:trimmedStr2 options:NSNumericSearch];
-                                         switch(result) {
-                                             case NSOrderedAscending:   return NSOrderedDescending;
-                                             case NSOrderedDescending:  return NSOrderedAscending;
-                                             case NSOrderedSame:        return NSOrderedSame;
-                                         }
-                                     });
-    
-    NSArray *sortedLineComponents = [lineComponents sortedArrayUsingComparator:compareFunction];
+    NSArray *sortedLineComponents = [lineComponents sortedArrayUsingComparator:self.compareFunction];
     NSString *sortedChunk         = [[sortedLineComponents componentsJoinedByString:@"\n"] stringByAppendingString:@"\n"];
     
     [textView.textStorage beginEditing];
