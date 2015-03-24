4c80bb2eb7830b36873281d5c89fbe7f273cae4a
Pedro Gomes <pdcgomes@gmail.com>
2015-03-24 19:11:27 +0000

    [ADD] First-pass column selection mode: sort of works, but only when selecting `downstream` and only ever covers two lines -- it's a start though!

--- PGXcodeActionBrowser/Model/Actions/XCColumnSelectionModeAction.m
+++ PGXcodeActionBrowser/Model/Actions/XCColumnSelectionModeAction.m
@@ -64,19 +64,83 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+- (BOOL)respondsToSelector:(SEL)aSelector
+{
+//    NSLog(@"<selector=%@", NSStringFromSelector(aSelector));
+
+    if(self.columnSelectionEnabled == NO) return NO;
+    
+    if(aSelector == @selector(textView:willChangeSelectionFromCharacterRanges:toCharacterRanges:)) {
+        return YES;
+    }
+    if(aSelector == @selector(textView:willChangeSelectionFromCharacterRange:toCharacterRange:)) {
+        return NO;
+    }
+    return [self.textViewDelegate respondsToSelector:aSelector];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 - (id)forwardingTargetForSelector:(SEL)aSelector
 {
     return self.textViewDelegate;
-
 }
 
-#pragma mark - XCColumnSelectionModeAction
+//1/////////////////////////////////////////////////////////////////////////////
+//2/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (NSArray *)textView:(NSTextView *)textView willChangeSelectionFromCharacterRanges:(NSArray *)oldSelectedCharRanges toCharacterRanges:(NSArray *)newSelectedCharRanges
+{
+    NSLog(@"<oldRanges=%@>, <newRanges=%@>", oldSelectedCharRanges, newSelectedCharRanges);
+
+    // FIXME: only supporting downstream selections for now, but shouldn't be to hard to support upstream
+    NSRange newSelectedCharRange = [newSelectedCharRanges.lastObject rangeValue];
+//
+    NSString *fullText = textView.string;
+    NSString *textEnclosedBySelection = [fullText substringWithRange:newSelectedCharRange];
+    
+    __block NSUInteger lineCount = 0;
+    [textEnclosedBySelection enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
+        lineCount++;
+    }];
+    
+    NSLog(@"<lines=%zd>", lineCount);
+    
+    if(lineCount < 2) return newSelectedCharRanges;
+
+    NSRange firstLineSelection    = [oldSelectedCharRanges.lastObject rangeValue];
+    NSRange lineRangeForSelection = [fullText lineRangeForRange:firstLineSelection];
+    NSArray *lineComponents       = [[fullText substringWithRange:lineRangeForSelection] componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSUInteger firstLineLength    = [lineComponents.firstObject length];
+    
+    NSUInteger lineStart;
+    NSUInteger lineEnd;
+    NSUInteger contentsEnd;
+    
+    [fullText getLineStart:&lineStart end:&lineEnd contentsEnd:&contentsEnd forRange:newSelectedCharRange];
+    
+    NSUInteger leadingOffset = (firstLineSelection.location - lineRangeForSelection.location);
+    NSUInteger nextLineStart = (lineStart +  firstLineLength + 1);
+    
+    // FIXME: account for prior zero length selection
+    NSRange nextLineColSelection = NSMakeRange(nextLineStart + leadingOffset, firstLineSelection.length);
+
+    NSMutableArray *columnSelectionRanges = newSelectedCharRanges.mutableCopy;
+    [columnSelectionRanges removeLastObject];
+    
+    [columnSelectionRanges addObject:[NSValue valueWithRange:firstLineSelection]];
+    [columnSelectionRanges addObject:[NSValue valueWithRange:nextLineColSelection]];
+
+    return columnSelectionRanges;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-//- (NSRange)textView:(NSTextView *)textView willChangeSelectionFromCharacterRange:(NSRange)oldSelectedCharRange toCharacterRange:(NSRange)newSelectedCharRange
-//{
-//    return newSelectedCharRange;
-//}
+- (NSRange)textView:(NSTextView *)textView willChangeSelectionFromCharacterRange:(NSRange)oldSelectedCharRange toCharacterRange:(NSRange)newSelectedCharRange
+{
+    
+    return newSelectedCharRange;
+}
 
 @end
