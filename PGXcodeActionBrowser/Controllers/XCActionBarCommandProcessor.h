9acbebdedf88639efe47764fc3bc715a135be408
Pedro Gomes <pdcgomes@gmail.com>
2015-04-02 16:55:21 +0100

    [ADD] CommandProcessor interface

---
+++ PGXcodeActionBrowser/Controllers/XCActionBarCommandProcessor.h
@@ -0,0 +1,27 @@
+//
+//  XCActionBarCommandProcessor.h
+//  XCActionBar
+//
+//  Created by Pedro Gomes on 02/04/2015.
+//  Copyright (c) 2015 Pedro Gomes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+@protocol XCActionBarCommandProcessor <NSObject>
+
+- (BOOL)enterActionSearchState;
+- (BOOL)enterActionArgumentState;
+
+- (BOOL)searchActionWithExpression:(NSString *)query;
+- (BOOL)autoCompleteWithSelectedAction;
+
+- (BOOL)selectNextSearchResult;
+- (BOOL)selectPreviousSearchResult;
+
+- (BOOL)executeSelectedAction;
+- (BOOL)executeSelectedActionWithArguments:(NSArray *)arguments;
+
+@end
