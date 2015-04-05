29a2edc5f80d914b5d317957a482d629606018af
Pedro Gomes <pdcgomes@gmail.com>
2015-04-05 20:21:07 +0100

    [IMPROVED] Supporting custom split delimiter

--- PGXcodeActionBrowser/Model/Actions/XCSplitSelectionIntoLinesAction.m
+++ PGXcodeActionBrowser/Model/Actions/XCSplitSelectionIntoLinesAction.m
@@ -26,8 +26,9 @@
 - (instancetype)init
 {
     if((self = [super init])) {
-        self.title    = @"Splits the selection into lines";
-        self.subtitle = @"Default deliminter: \",\", can be optionally specified";
+        self.title        = NSLocalizedString(@"Splits the selection into lines", @"");
+        self.subtitle     = NSLocalizedString(@"Default delimiter: \",\", can be optionally specified", @"");
+        self.argumentHint = NSLocalizedString(@"Specify the desired delimiter", @"");
         self.enabled  = YES;
     }
     return self;
@@ -44,23 +45,40 @@
 ////////////////////////////////////////////////////////////////////////////////
 - (BOOL)validateArgumentsWithContext:(id<XCIDEContext>)context arguments:(NSString *)arguments
 {
-    return NO;
+    return arguments.length > 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 - (BOOL)executeWithContext:(id<XCIDEContext>)context
 {
-    NSString *fullText           = [context.sourceCodeTextView string];
-    NSArray  *rangesForSelection = [context retrieveTextSelectionRanges];
+    return [self splitSelectionInContext:context delimiter:@","];
+}
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (BOOL)executeWithContext:(id<XCIDEContext>)context arguments:(NSString *)arguments
+{
+    return [self splitSelectionInContext:context delimiter:arguments];
+}
+
+#pragma mark - Helpers
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (BOOL)splitSelectionInContext:(id<XCIDEContext>)context delimiter:(NSString *)delimiter
+{
+    NSString *fullText             = [context.sourceCodeTextView string];
+    NSArray  *rangesForSelection   = [context retrieveTextSelectionRanges];
+    NSString *newLineWithDelimiter = [NSString stringWithFormat:@"%@\n", delimiter];
+    
     [context.sourceCodeDocument.textStorage beginEditing];
 
     [rangesForSelection enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(NSValue *value, NSUInteger idx, BOOL *stop) {
         NSRange range = value.rangeValue;
-    
+        
         NSString *textSelection = [fullText substringWithRange:range];
-        NSString *textSelectionSplitByDelimiter = [textSelection stringByReplacingOccurrencesOfString:@"," withString:@",\n"];
+        NSString *textSelectionSplitByDelimiter = [textSelection stringByReplacingOccurrencesOfString:delimiter withString:newLineWithDelimiter];
         
         [context.sourceCodeDocument.textStorage replaceCharactersInRange:range
                                                               withString:textSelectionSplitByDelimiter
@@ -73,13 +91,7 @@
     [context.sourceCodeDocument.textStorage endEditing];
     
     return YES;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-- (BOOL)executeWithContext:(id<XCIDEContext>)context arguments:(NSString *)arguments
-{
-    return NO;
+    
 }
 
 @end
