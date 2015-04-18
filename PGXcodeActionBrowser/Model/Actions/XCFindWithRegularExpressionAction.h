53ceb802d526dbb936fd4f5501904593d9170130
Pedro Gomes <pdcgomes@gmail.com>
2015-04-18 13:01:29 +0100

    [ADD] FindWithRegularExpressionAction to wrap up XCFindWithExpressionAction presets

---
+++ PGXcodeActionBrowser/Model/Actions/XCFindWithRegularExpressionAction.h
@@ -0,0 +1,13 @@
+//
+//  XCFindWithRegularExpressionAction.h
+//  XCActionBar
+//
+//  Created by Pedro Gomes on 18/04/2015.
+//  Copyright (c) 2015 Pedro Gomes. All rights reserved.
+//
+
+#import "XCCustomAction.h"
+
+@interface XCFindWithRegularExpressionAction : XCCustomAction
+
+@end
---
+++ PGXcodeActionBrowser/Model/Actions/XCFindWithRegularExpressionAction.m
@@ -0,0 +1,13 @@
+//
+//  XCFindWithRegularExpressionAction.m
+//  XCActionBar
+//
+//  Created by Pedro Gomes on 18/04/2015.
+//  Copyright (c) 2015 Pedro Gomes. All rights reserved.
+//
+
+#import "XCFindWithRegularExpressionAction.h"
+
+@implementation XCFindWithRegularExpressionAction
+
+@end
